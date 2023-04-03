6f48a4f659fc3171e9777c6894b11de55762f955
solomiya1tryasko <solomiya1tryasko@gmail.com>
2023-04-03 13:05:30 +0300

    removed spacer

--- lib/rows.dart
+++ lib/rows.dart
@@ -14,7 +14,6 @@ class Mediateka extends StatelessWidget {
             style: TextStyle(
                 fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
           ),
-          Spacer(),
           CircleAvatar(
               backgroundColor: Colors.red,
               backgroundImage: NetworkImage(
