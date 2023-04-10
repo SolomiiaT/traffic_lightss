68b772b7c72af5860c718aa01a79db4a7b40b762
SolomiiaT <129739245+SolomiiaT@users.noreply.github.com>
2023-04-10 14:15:59 +0300

    new page

---
+++ lib/home_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'package:traffic_lightss/favourite_page.dart';
+import 'package:traffic_lightss/second_page/album_page.dart';
+import 'package:traffic_lightss/viewing_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    ViewingPage(),
+    AlbumPage(),
+    FavouritePage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
